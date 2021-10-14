04673757d1ed667c1daa628d8f5e8b2c46ff5d7e
Aluciney Wanderley <aluciney.wanderley@gmail.com>
2021-10-13 23:28:28 -0400

    Rename Middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { DateController } from './controllers/DateController';
-import ValidateOptionsMidlleware from './middlewares/ValidateOptionsMidlleware';
+import ValidateOptionsMiddleware from './middlewares/ValidateOptionsMiddleware';
 
 const routes = Router();
 
-routes.get('/date_range', ValidateOptionsMidlleware, DateController.index);
+routes.get('/date_range', ValidateOptionsMiddleware, DateController.index);
 
 export default routes;
